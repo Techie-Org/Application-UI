74115884f1bb0afe55fa0a508ab6a90a87e08fee
Mohd Azam <78146637+Azam4204@users.noreply.github.com>
2024-10-16 19:59:48 +0530

    [Aazam]-[Refactored-App-Structure-Added-eslint-Rules] (#7)
    * [Aazam]-Refactored the app structure and added eslint rules
    
    * [Aazam]-Fixed code breaking style
    
    * [Aazam]-Added eslint rules and modified import resolver

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
 const app = express();
-const webpackConfig = require('../../webpack.config.js');
+const webpackConfig = require('../webpack.config.js');
 const compiler = webpack(webpackConfig);
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js file as base
@@ -22,9 +22,11 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, '../../dist'))); // TODO: need to modify the path
+app.use(express.static(path.join(__dirname, '../dist'))); // TODO: need to modify the path
 
+//Since webpackDevMiddleware uses memory-fs internally to store build artifacts, we use it instead
 const fs = middleware.context.outputFileSystem;
+
 app.get('*', (req, res) => {
   fs.readFile(path.join(compiler.outputPath, 'index.html'), (err, file) => {
     if (err) {
@@ -37,7 +39,7 @@ app.get('*', (req, res) => {
 
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
