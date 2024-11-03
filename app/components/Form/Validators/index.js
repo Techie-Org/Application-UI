96b9fb57cd27c37a550c1687857804e73871bcf3
Mohd Azam <78146637+Azam4204@users.noreply.github.com>
2024-11-03 20:57:46 +0530

    [Aazam]-[UserAccount Added-SignIn-SignUp] (#1)
    * [Aazam]-[UserAccount Added-SignIn-SignUp]
    
    * [Aazam]-Added disable state on SignUp button
    
    * [Aazam]-Fixed forms avatar background color
    
    * [Aazam]-Added validators in the form fields
    
    * [Aazam]-Added password matching error validation
    
    * [Aazam]-Added validators to signIn form

---
+++ app/components/Form/Validators/index.js
@@ -0,0 +1,19 @@
+import { isString } from 'lodash';
+import { EMAIL_ADDRESS_ALLOWED_CHARS } from '../Util';
+import { ENGLISH_LETTERS_PATTERN } from '../Util/constants';
+
+export const isRequiredValidator = (message) => (val) =>
+  val && !!(val?.length ?? true) ? undefined : message;
+
+export const isBlankValidator = (message) => (string) =>
+  !isString(string) || string?.trim().length === 0 ? message : '';
+
+export const confirmPasswordValidator =
+  (message) => (val, formValues, instanceRef) =>
+    val === formValues.get(instanceRef.form).toJS().password ? '' : message;
+
+export const NAME_PATTERN = ENGLISH_LETTERS_PATTERN;
+
+export const EMAIL_ALLOWED_PATTERNS = EMAIL_ADDRESS_ALLOWED_CHARS;
+
+export const PHONE_NUMBER_PATTERN = /^[0-9]*$/;
