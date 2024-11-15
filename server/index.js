a822ded3b7a17327130f6979c94bb65746eceea0
Mohd Azam <78146637+Azam4204@users.noreply.github.com>
2024-11-15 20:39:47 +0530

    [Aazam]-Added backend API server url (#28)

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const app = express();
 const webpackConfig = require('../webpack.config');
 const compiler = webpack(webpackConfig);
 
-const pxhost = process.env.npm_config_pxhost || 'http://localhost:5000/api'; // Added api with base as pathRewrite not working in proxy middleware
+// const pxhost = process.env.npm_config_pxhost || 'http://localhost:5000';
+const pxhost = process.env.npm_config_pxhost || 'https://dev-kalakaar.onrender.com';
 
 app.use(
   '/api',
