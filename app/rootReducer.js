4c819546bc523b9e936c18d4734a861bbbd52302
Mohd Azam <78146637+Azam4204@users.noreply.github.com>
2024-11-19 18:45:57 +0530

    [Aazam]-Added global-header-reducer-saga-to-store (#24)

--- app/rootReducer.js
+++ app/rootReducer.js
@@ -4,10 +4,12 @@
 
 import routerReducer from 'utils/routeReducer';
 import appReducer from 'containers/App/reducer';
+import globalHeaderReducer from 'containers/GlobalHeaderContainer/reducer';
 
 export const rootReducer = {
   route: routerReducer,
   app: appReducer,
+  globalHeader: globalHeaderReducer,
 };
 
 export default rootReducer;
