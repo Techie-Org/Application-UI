b3abb6a1ee7598a4e5991558a2bc6efd5cc33092
Aazam <Azam812613@gmail.com>
2024-12-14 13:29:55 +0530

    [Aazam]-[UserAccount Fixed-Email-CaseInsensitive-SignIn-SignUp-Payload]

--- app/containers/SignIn/actions.js
+++ app/containers/SignIn/actions.js
@@ -1,9 +1,10 @@
+import { parseSignInFormData } from './utils';
 import { SIGN_IN_USER, SIGN_IN_USER_SUCCESS } from './constants';
 
 export function signInUser(signInFormData) {
   return {
     type: SIGN_IN_USER,
-    formData: signInFormData,
+    formData: parseSignInFormData(signInFormData),
   };
 }
 
