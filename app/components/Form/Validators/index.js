1f258fbd8dc53458b6dad4cc47243eea67b2450a
Shaanthinithiyagarajan <shaanthini0504@gmail.com>
2024-12-15 18:18:25 +0530

    removed otp length validator

--- app/components/Form/Validators/index.js
+++ app/components/Form/Validators/index.js
@@ -19,4 +19,3 @@ export const lengthCheckValidator = (message, min = 0, max = 25) => (value) => v
 export const emailValidator = (message) => (value) => value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
   ? message
   : '';
-export const otpLengthValidator = (message) => (value) => value && value.length === 6 ? '' : message;
