b13e64b8077e42378f2ae2bcc21ea8f94a29032e
Shaanthinithiyagarajan <shaanthini0504@gmail.com>
2024-12-15 13:37:15 +0530

    fix the change

--- app/containers/SignUp/actions.js
+++ app/containers/SignUp/actions.js
@@ -18,20 +18,12 @@ export function registerUser(signUpFormData) {
 }
 
 export function registerUserSuccess(data) {
-  console.log(
-    'setup containers/SignUp registerUserSuccess action called dat',
-    data
-  );
   return {
     type: REGISTER_USER_SUCCESS,
     data,
   };
 }
 export function validateOtp(otp) {
-  console.log(
-    'setup containers/Signin ootp validation action called data',
-    otp
-  );
   return {
     type: VALIDATE_OTP,
     otpValue: otp,
@@ -39,10 +31,6 @@ export function validateOtp(otp) {
 }
 
 export function validateOtpSuccess(data) {
-  console.log(
-    'setup containers/SignIn validateOtpSuccess action called data',
-    data
-  );
   return {
     type: VALIDATE_OTP_SUCCESS,
     data,
