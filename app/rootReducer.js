fa8e48f267872839cd6dbcd700c7d0e603fc0797
Mohd Azam <78146637+Azam4204@users.noreply.github.com>
2024-12-21 12:44:59 +0530

    Revert "[Aazam]-Added global-header-reducer-saga-to-store (#24)" (#51)
    This reverts commit 4c819546bc523b9e936c18d4734a861bbbd52302.

--- app/rootReducer.js
+++ app/rootReducer.js
@@ -4,12 +4,10 @@
 
 import routerReducer from 'utils/routeReducer';
 import appReducer from 'containers/App/reducer';
-import globalHeaderReducer from 'containers/GlobalHeaderContainer/reducer';
 
 export const rootReducer = {
   route: routerReducer,
   app: appReducer,
-  globalHeader: globalHeaderReducer,
 };
 
 export default rootReducer;
